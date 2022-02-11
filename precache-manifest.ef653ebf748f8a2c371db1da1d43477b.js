0ece4d0f609db6cdf7ebcc2ece40dff3732209ff
杨绅轩 <957355473@qq.com>
2022-02-11 19:38:21 +0800

    deploy

---
+++ precache-manifest.ef653ebf748f8a2c371db1da1d43477b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9937e0d4b0f1cd1a107b",
+    "url": "/morney-1-website/css/app.fb208b3b.css"
+  },
+  {
+    "revision": "5cbba78b76fd4489974fb9fe81a5d408",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "9937e0d4b0f1cd1a107b",
+    "url": "/morney-1-website/js/app.47d2c3fd.js"
+  },
+  {
+    "revision": "94773fd83ccf013e1dd4",
+    "url": "/morney-1-website/js/chunk-vendors.03f31b6a.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "2c86e44f5ee95ccc24b06747e362fd31",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
